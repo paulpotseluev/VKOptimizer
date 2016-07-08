e2bf42f484f694b9fe343c86fe8eb0c8cea66965
azimutalius <p@codeit.org.ua>
2016-07-08 18:01:34 +0300

    fixed round avatars v2

--- VKoptimizer.round.user.js
+++ VKoptimizer.round.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://vk.com/
 // @updateURL   https://github.com/paulpotseluev/VKOptimizer/raw/master/VKoptimizer.user.js
 // @downloadURL https://github.com/paulpotseluev/VKOptimizer/raw/master/VKoptimizer.user.js
-// @version      0.43.7
+// @version      0.43.8.5
 // @description  Скрипт, изменяющий внешний вид страницы ВКонтакте с новым дизайном для более удобного восприятия.
 // @author       Paul Potseluev
 // @match        https://new.vk.com/*
@@ -43,7 +43,7 @@ addGlobalStyle('.friends_user_row .online:after {border: 2px solid #fff}');
 //Исправляем счётчик сообщений в чате
 addGlobalStyle('.chat_tab_counter {top: 6px;left: 6px;height: 8px;min-width: 5px;border-radius: 6px;padding: 3px 3px;font-size: 11px;}');
 //Убираем круглые аватарки и прочие элементы
-addGlobalStyle('.nim-peer--photo-w,.nim-dialog.nim-dialog_classic .nim-dialog--photo .nim-peer .nim-peer--photo .im_grid>img:after, .nim-dialog.nim-dialog_classic .nim-dialog--photo .nim-peer .nim-peer--photo>img:after,.nim-peer .nim-peer--photo .im_grid>img, .nim-peer .nim-peer--photo>img,.top_profile_img,.reply_img,.copy_post_img,.post_img,.cell_img,.people_cell_img,.group_box_image_link,.cell_img,.thumb,.poll_tt_image,.like_tt_image,.group_row_photo,.right_list_img,.page_video_play_icon,.chat_tab_img,.profile_career_img,.mention_tt_img,.audio_page_player_play.icon,.ow_ava.ow_ava_mid,.feedback_img,.friends_common_img,.notifier_image {border-radius:10% !important}');
+addGlobalStyle('.im_grid>img,.nim-peer--photo-w,.top_profile_img,.reply_img,.copy_post_img,.post_img,.cell_img,.people_cell_img,.group_box_image_link,.cell_img,.thumb,.poll_tt_image,.like_tt_image,.group_row_photo,.right_list_img,.page_video_play_icon,.chat_tab_img,.profile_career_img,.mention_tt_img,.audio_page_player_play.icon,.ow_ava.ow_ava_mid,.feedback_img,.friends_common_img,.notifier_image {border-radius:10% !important}');
 addGlobalStyle('.audio_play {border-radius:10% !important; width: 20px !important; height: 20px !important; background: #577ca1 url(/images/icons/audio_playpause.png) no-repeat 6px 5px !important; margin-top:2px !important}');
 addGlobalStyle('a.fc_msgs_img,img.fc_contact_photo,.mention_tt_person_img,.thumb_off,.thumb_on,.ow_ava.ow_ava_small,.feedback_group_photo_img,.audio_page_player_play .icon {border-radius:15% !important}');
 addGlobalStyle('.friends_photo_img,.fans_fan_img,.group_row_img {border-radius:5% !important}');
